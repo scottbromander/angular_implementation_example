857ace404721ce36a55f77bbe8bf2f7f425a0923
Scott Bromander <scott@primeacademy.io>
2015-11-24 14:57:13 -0600

    added GitHub

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,20 +1,12 @@
 var app = angular.module('StarterApp', ['ngMaterial', 'ngMessages']);
 
-app.controller('AppCtrl', ['$scope', '$mdDialog', function($scope, $mdDialog){
-    var alert;
-    $scope.showDialog = function($event) {
-        alert = $mdDialog.alert({
-            title: 'Awesome Popup',
-            content: 'MODAL!!!!!',
-            ok: 'Close'
-        });
-
-        $mdDialog
-            .show( alert )
-            .finally(function() {
-                alert = undefined;
-            });
-    };
+app.config(['$mdThemingProvider', function($mdThemingProvider){
+    $mdThemingProvider.theme('default')
+        .primaryPalette('blue-grey')
+        .accentPalette('grey');
 }]);
 
+app.controller('AppCtrl', ['$scope', '$mdDialog', function($scope, $mdDialog){
+
+}]);
 
